5eee56aff927aa3139cdf8d5798cb95cacbe34cd
wongchakman1308 <78790278+wongchakman1308@users.noreply.github.com>
2021-04-25 16:37:52 +0800

    Create printout.cpp

---
+++ printout.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "main.h"
+#include <unistd.h>
+#include <string>
+using namespace std;
+
+void printout(string paragraph){
+    int x = 0;
+    while (paragraph[x] != '\0'){
+        cout << paragraph[x++];
+        cout.flush();
+        usleep(20000); // 10000
+    }
+}
