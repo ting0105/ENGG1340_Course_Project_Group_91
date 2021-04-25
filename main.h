70096616b8548595177eb5da5f128f20242191b5
ting0105 <78613790+ting0105@users.noreply.github.com>
2021-04-25 11:48:36 +0800

    Update main.h

--- main.h
+++ main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void stat_gen(int arr[7]);
+void stat_gen(int arr[10]);
+bool combat(int * monsterHP, int * monsterDEX, int * monsterSTR, int &playerHP, int playerDEX, int playerSTR, int playerSIZ, bool kit_getknife);
+
 
 #endif
