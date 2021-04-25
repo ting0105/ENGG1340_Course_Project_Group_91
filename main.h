9633af549ea4ee929befe39406fe55d4a0e45d01
wongchakman1308 <78790278+wongchakman1308@users.noreply.github.com>
2021-04-25 19:35:05 +0800

    Update main.h

--- main.h
+++ main.h
@@ -1,8 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <string>
+using namespace std;
 
 void stat_gen(int arr[10]);
 bool combat(int * monsterHP, int * monsterDEX, int * monsterSTR, int &playerHP, int playerDEX, int playerSTR, int playerSIZ, bool kit_getknife);
-
+void printout(string paragraph);
 
 #endif
